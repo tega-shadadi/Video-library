bf194be1ac2880daeffaf34849a0af6d8851e561
Shadadi <brynkeener12@gmail.com>
2026-04-16 10:43:11 +0300

    Add membership form with validation and enhance movie selection functionality

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+const movies = document.querySelectorAll(".movie-card");
+
+movies.forEach(movie => {
+    movie.addEventListener("click", function () {
+        const title = this.querySelector("h3").innerText;
+        console.log("Selected movie: " + title);
+        alert("You selected: " + title);
+    });
+});
+
+
+// MEMBERSHIP FORM VALIDATION
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.getElementById("membershipForm");
+
+    if (form) {
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            const fullname = document.getElementById("fullname").value;
+            const email = document.getElementById("email").value;
+            const phone = document.getElementById("phone").value;
+            const membershipType = document.getElementById("membershipType").value;
+
+            if(fullname === "" || email === ""){
+                alert(" Please fill in all required fields!");
+            }
+            else{
+                alert(
+                    " Registration Successful!\n\n" +
+                    "Name: " + fullname + "\n" +
+                    "Email: " + email + "\n" +
+                    "Phone: " + phone + "\n" +
+                    "Type: " + membershipType
+                );
+            }
+        });
+    }
+
+});
+
+
+
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
 toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+
+
+
+
